531118b67884cd119df87d2ce3eea0e35e51b705
Elham-EN <ilhamrezaie@gmail.com>
2023-12-09 09:42:38 +1100

    Created Item Entity for Mongodb

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,5 +15,16 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// MongoDB Initialization
+await DB.InitAsync("SearchServiceDB", MongoClientSettings
+    .FromConnectionString(builder
+        .Configuration.GetConnectionString("MongoDbConnection")));
+// Create index for the Item (for cenrtain fields) to be able to search on
+await DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
